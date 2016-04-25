795865052ce095a99548f742984699452ac4f05b
amitbed <amitbed@post.bgu.ac.il>
2016-04-25 12:33:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,23 @@
  */
 var https = require('https');
 var port = process.env.PORT || 443;;
+var name = 0 ;
+var ts = 0 ;
+var metadata = 0 ;
+var state = 0;
+var id = 0;
 
 function onRequest(request, response){
     console.log("Neura has made a request: " + request.url);
     if (request.method == 'POST'){
         response.writeHead(200, {"version" : "HTTP/1.1" ,
                                  "Content-Type": "application/json"});
-        //write in a web browser
-        response.write("Name: " + JSON.stringify(request.name));
-        response.write("Timestamp: " + JSON.stringify(request.timestamp));
-        response.write("Metadata: " + JSON.stringify(request.metadata));
-        response.write("State: " + JSON.stringify(request.state));
-        response.write("Identifier of subscription: " + JSON.stringify(request.identifier));
+
+        name = JSON.stringify(request.name);
+        ts = JSON.stringify(request.timestamp);
+        metadata = JSON.stringify(request.metadata);
+        state = JSON.stringify(request.state);
+        id = JSON.stringify(request.identifier);
 
         console.log("Name: " + JSON.stringify(request.name));
         console.log("Timestamp: " + JSON.stringify(request.timestamp));
@@ -23,6 +28,13 @@ function onRequest(request, response){
         console.log("Identifier of subscription: " + JSON.stringify(request.identifier));
 
         response.end();
+    }else{
+        //write in a web browser
+        response.write("Name: " + name);
+        response.write("Timestamp: " + ts);
+        response.write("Metadata: " + metadata);
+        response.write("State: " + state);
+        response.write("Identifier of subscription: " + id);
     }
 }
 
