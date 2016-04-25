7d70d6691ec96d0c4fa83725d051adc75daa700e
amitbed <amitbed@post.bgu.ac.il>
2016-04-25 11:50:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ var port = process.env.PORT || 443;;
 function onRequest(request, response){
     console.log("Neura has made a request: " + request.url);
     if (request.method == 'POST'){
-        response.writeHead(200, {"Content-Type": "application/json"});
-        console.log(request.name);
+        response.writeHead(200, {"version" : "HTTP/1.1" ,
+                                 "Content-Type": "application/json"});
+        //write in a web browser
+        response.write("Name: " + JSON.stringify(request.name));
+        response.write("Timestamp: " + JSON.stringify(request.timestamp));
+        response.write("Metadata: " + JSON.stringify(request.metadata));
+        response.write("State: " + JSON.stringify(request.state));
+        response.write("Identifier of subscription: " + JSON.stringify(request.identifier));
+
+        console.log("Name: " + JSON.stringify(request.name));
+        console.log("Timestamp: " + JSON.stringify(request.timestamp));
+        console.log("Metadata: " + JSON.stringify(request.metadata));
+        console.log("State: " + JSON.stringify(request.state));
+        console.log("Identifier of subscription: " + JSON.stringify(request.identifier));
+
         response.end();
     }
 }
